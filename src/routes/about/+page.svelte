25485ae88f6e476472ccec602048246657ae22cb
alyx <alexanderbek3@gmail.com>
2024-08-27 20:29:14 +0930

    add about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">About</h1>
+		<p>Bland content coming soon</p>
+		<p>Currently built using Svelte and Skeleton.</p>
+	</div>
+</div>
