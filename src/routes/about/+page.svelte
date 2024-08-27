eafd55eb1f589b7193d08fd6eb6a678a2f32161b
alyx <alexanderbek3@gmail.com>
2024-08-27 20:38:50 +0930

    about written

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,9 @@
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full mx-auto flex p-10">
 	<div class="space-y-5">
 		<h1 class="h1">About</h1>
-		<p>Bland content coming soon</p>
-		<p>Currently built using Svelte and Skeleton.</p>
+		<p>
+			Anything written here is entirely a work of fiction. Any resemblance to real life is purely
+			inevitable
+		</p>
 	</div>
 </div>
