55f6b7bae9daf8c9919d0f040ff69d4777fbb23d
alyx <alexanderbek3@gmail.com>
2024-08-27 17:59:56 +0930

    updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
 	import '../app.postcss';
+	import { AppBar, AppRail, AppRailTile } from '@skeletonlabs/skeleton';
+
+	let currentTile: number = 0;
 </script>
 
-<slot />
+<div class="grid h-screen grid-rows-[auto_1fr_auto]">
+	<header class="sticky p-1">
+		<AppBar>
+			<svelte:fragment slot="headline">Scylla's Blog</svelte:fragment>
+		</AppBar>
+	</header>
+	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
+		<aside class="sticky p-1">
+			<AppRail>
+				<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="Posts">
+					<span>Posts</span>
+				</AppRailTile>
+			</AppRail>
+		</aside>
+		<main class="space-y-4 p-1">
+			<slot />
+		</main>
+	</div>
+</div>
