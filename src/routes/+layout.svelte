b883fc51ccbae6f27e837d093baddbc4747c2f70
alyx <alexanderbek3@gmail.com>
2024-09-01 20:42:48 +0930

    add a drawer for nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,71 +1,84 @@
 <script lang="ts">
 	import '../app.postcss';
 	import '$lib/GitHubLogo.svelte';
+	import '@fortawesome/fontawesome-free/css/fontawesome.css';
+	import '@fortawesome/fontawesome-free/css/brands.css';
+	import '@fortawesome/fontawesome-free/css/solid.css';
 	import {
 		AppBar,
 		AppRail,
-		LightSwitch,
 		AppRailAnchor,
-		TabGroup,
-		TabAnchor
+		initializeStores,
+		Drawer,
+		LightSwitch,
+		getDrawerStore
 	} from '@skeletonlabs/skeleton';
-
+	import type { AppRailTile, DrawerSettings, DrawerStore } from '@skeletonlabs/skeleton';
 	import { page } from '$app/stores';
 	import GitHubLogo from '$lib/GitHubLogo.svelte';
+
+	initializeStores();
+
 	let currentTile: number = 0;
-	let currentTab: number = 0;
+
+	const drawerStore = getDrawerStore();
+
+	const drawerSettings: DrawerSettings = {
+		id: 'navigationMenu',
+		width: 'w-[120px]'
+	};
+
+	function openMenu() {
+		drawerStore.open(drawerSettings);
+	}
 
 	export let data;
 </script>
 
 <title>Confused Ramblings</title>
+<Drawer position="left">
+	<AppRail>
+		<AppRailAnchor
+			bind:group={currentTile}
+			selected={$page.url.pathname === '/about'}
+			name="about-tile"
+			value={0}
+			href="/about"
+		>
+			<span>About</span>
+		</AppRailAnchor>
+		<AppRailAnchor
+			bind:group={currentTile}
+			selected={$page.url.pathname === '/'}
+			name="posts-tile"
+			value={1}
+			href="/"
+		>
+			<span>Posts</span>
+		</AppRailAnchor>
+	</AppRail>
+</Drawer>
 <div class="grid h-screen grid-rows-[auto_1fr_auto]">
 	<header class="sticky p-1">
-		<AppBar slotTrail="place-content-end" gridColumns="grid-cols-3">
+		<AppBar
+			slotTrail="place-content-end"
+			slotDefault="place-content-evenly"
+			gridColumns="grid-cols-3"
+		>
 			<svelte:fragment slot="lead">
-				<a href="/">Scylla's Blog</a>
+				<button type="button" class="btn btn-lg variant-filled" on:click={openMenu}
+					><i class="fa-solid fa-bars text-2xl" /></button
+				>
 			</svelte:fragment>
-			<TabGroup
-				justify="justify-centre"
-				active="variant-filled-primary"
-				hover="hover:variant-soft-primary"
-				flex="flex-1 lg:flex-none"
-				rounded=""
-				border="border-b border-surface-400-500-token"
-				class="bg-surface-200-700-token w-full"
-			>
-				<TabAnchor
-					bind:group={currentTab}
-					href="/about"
-					name="tile-about"
-					value={1}
-					selected={$page.url.pathname === '/about'}
-					title="About"
-				>
-					<span>About</span>
-				</TabAnchor>
-				<TabAnchor
-					href="/posts"
-					bind:group={currentTab}
-					name="tile-posts"
-					value={0}
-					title="Posts"
-					selected={$page.url.pathname === '/posts'}
-				>
-					<span>Posts</span>
-				</TabAnchor>
-			</TabGroup>
+			<span>Incoherent Ramblings</span>
 			<svelte:fragment slot="trail">
-					<div>
-						<LightSwitch />
-					</div>
-					<div>
-						<GitHubLogo
-							viewBoxSetting={'0 0 100 100'}
-							scale=".6"
-						></GitHubLogo>
-					</div>
-				</svelte:fragment>
+				<div>
+					<LightSwitch />
+				</div>
+				<div>
+					<GitHubLogo viewBoxSetting={'0 0 100 100'} scale=".6"></GitHubLogo>
+				</div>
+			</svelte:fragment>
 		</AppBar>
 	</header>
 	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
