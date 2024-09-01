ded135475b1f95424d4240cd969f1a9ea04111d9
alyx <alexanderbek3@gmail.com>
2024-09-01 20:42:33 +0930

    add shiki

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from "mdsvex";
+import { mdsvex, escapeSvelte } from "mdsvex";
+
+import { createHighlighter } from 'shiki';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = "text") => {
+			const highlighter = await createHighlighter({
+				themes: ['poimandres'],
+				langs: ['javascript', 'typescript', 'python']
+			})
+			await highlighter.loadLanguage('javascript', 'typescript', 'python')
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
+			return `{@html \`${html}\` }`
+		}
+	}
 }
 
 /** @type {import('@sveltejs/kit').Config} */
