336a7b25080223f48081dbf89a53d06b5a97044a
alyx <alexanderbek3@gmail.com>
2024-09-01 10:21:56 +0930

    remove app rail

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
 	import '../app.postcss';
 	import '$lib/GitHubLogo.svelte';
-	import { AppBar, AppRail, AppRailTile, LightSwitch, AppRailAnchor } from '@skeletonlabs/skeleton';
+	import {
+		AppBar,
+		AppRail,
+		LightSwitch,
+		AppRailAnchor,
+		TabGroup,
+		TabAnchor
+	} from '@skeletonlabs/skeleton';
 
 	import { page } from '$app/stores';
 	import GitHubLogo from '$lib/GitHubLogo.svelte';
 	let currentTile: number = 0;
+	let currentTab: number = 0;
 
 	export let data;
 </script>
@@ -13,18 +21,21 @@
 <title>Confused Ramblings</title>
 <div class="grid h-screen grid-rows-[auto_1fr_auto]">
 	<header class="sticky p-1">
-		<AppBar slotTrail="place-content-end">
-			<a href="/">Scylla's Blog</a>
-			<svelte:fragment slot="trail">
-				<LightSwitch />
+		<AppBar slotTrail="place-content-end" gridColumns="grid-cols-3">
+			<svelte:fragment slot="lead">
+				<a href="/">Scylla's Blog</a>
 			</svelte:fragment>
-		</AppBar>
-	</header>
-	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
-		<aside class="sticky p-1">
-			<AppRail>
-				<AppRailAnchor
-					bind:group={currentTile}
+			<TabGroup
+				justify="justify-centre"
+				active="variant-filled-primary"
+				hover="hover:variant-soft-primary"
+				flex="flex-1 lg:flex-none"
+				rounded=""
+				border="border-b border-surface-400-500-token"
+				class="bg-surface-200-700-token w-full"
+			>
+				<TabAnchor
+					bind:group={currentTab}
 					href="/about"
 					name="tile-about"
 					value={1}
@@ -32,26 +43,37 @@
 					title="About"
 				>
 					<span>About</span>
-				</AppRailAnchor>
-				<AppRailAnchor
+				</TabAnchor>
+				<TabAnchor
 					href="/posts"
-					bind:group={currentTile}
+					bind:group={currentTab}
 					name="tile-posts"
 					value={0}
 					title="Posts"
 					selected={$page.url.pathname === '/posts'}
 				>
 					<span>Posts</span>
-				</AppRailAnchor>
-
-				<svelte:fragment slot="trail">
-					<AppRailAnchor href="https://github.com/thembo-sec" title="GitHub">
-						<GitHubLogo></GitHubLogo>
-						<span>GitHub</span>
-					</AppRailAnchor>
-				</svelte:fragment>
-			</AppRail>
-		</aside>
+				</TabAnchor>
+			</TabGroup>
+			<svelte:fragment slot="trail">
+				<div class="grid grid-cols-4">
+					<div></div>
+					<div>
+						<LightSwitch />
+					</div>
+					<div>
+						<GitHubLogo
+							viewBoxSetting={'-400 -25 550 150'}
+							scale="2"
+							heightSetting={'100%'}
+							widthSetting={'100%'}
+						></GitHubLogo>
+					</div>
+				</div></svelte:fragment
+			>
+		</AppBar>
+	</header>
+	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
 		<main class="space-y-4 p-1">
 			<slot />
 		</main>
