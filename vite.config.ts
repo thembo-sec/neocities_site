58a97153c69979149d7d0207ca40e087111f94cf
alyx <alexanderbek3@gmail.com>
2024-09-02 17:12:28 +0930

    add rss generator

--- vite.config.ts
+++ vite.config.ts
@@ -4,38 +4,35 @@ import { defineConfig } from 'vite';
 import type { Post } from '$lib/types'
 import type { Plugin, ResolvedConfig } from 'vite';
 import path from 'path';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import fm from 'yaml-front-matter';
 import fs from 'fs-extra';
-
 
 function writeRss(): Plugin {
 	let config: ResolvedConfig;
 
 	let posts: Post[] = [];
 
-	const xml = `
-		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
-			<channel>
-				<title>Ash's Incoherrent rambling</title>
-				<description>Random Thoughts</description>
-				<link>https://scylla.neocities.org</link>
-				<atom:link href="https://scylla.neocities.org/rss.xml" rel="self" type="application/rss+xml"/>
-				${posts
-			.map(
-				(post) => `
-						<item>
-							<title>${post.title}</title>
-							<description>${post.description}</description>
-							<link>https://scylla.neocities.org/${post.slug}</link>
-							<guid isPermaLink="true">https://scylla.neocities.org/${post.slug}</guid>
-							<pubDate>${new Date(post.date).toUTCString()}</pubDate>
-						</item>
-					`
-			)
-			.join('')}
-			</channel>
-		</rss>
-	`.trim()
+	const mdPath = path.resolve('src', 'posts');
+
+	let files = fs.readdirSync(mdPath);
+
+	files.forEach(file => {
+		const data = fs.readFileSync(path.resolve('src', 'posts', file));
+		let content = fm.loadFront(data);
+		const post: Post = {
+			title: content.title,
+			description: content.description,
+			categories: content.categories,
+			date: content.date,
+			slug: content.slug,
+			edited: content.edited,
+			published: content.published
+		};
+		posts.push(post);
+
+	})
+
+	console.log(posts);
 
 	return {
 		name: 'rss-write',
@@ -46,6 +43,36 @@ function writeRss(): Plugin {
 		},
 		writeBundle() {
 			const filePath = path.resolve(config.root, 'static', 'rss.xml');
+
+
+
+
+			const xml = `
+				<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+					<channel>
+						<title>Ash's Incoherrent rambling</title>
+						<description>Random Thoughts</description>
+						<link>https://scylla.neocities.org</link>
+						<atom:link href="https://scylla.neocities.org/rss.xml" rel="self" type="application/rss+xml"/>
+						${posts
+					.map(
+						(post) => `
+								<item>
+									<title>${post.title}</title>
+									<description>${post.description}</description>
+									<link>https://scylla.neocities.org/${post.slug}</link>
+									<guid isPermaLink="true">https://scylla.neocities.org/${post.slug}</guid>
+									<pubDate>${new Date(post.date).toUTCString()}</pubDate>
+								</item>
+							`
+					)
+					.join('')}
+					</channel>
+				</rss>
+			`.trim();
+
+			console.log(posts);
+
 			fs.ensureFileSync(filePath);
 			fs.writeFile(filePath, xml);
 		}
