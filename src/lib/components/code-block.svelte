c47728f86bc2b23cfd9dc655fe900c20aa336b48
alyx <alexanderbek3@gmail.com>
2026-01-12 10:29:50 +1030

    add codeblock for future use

---
+++ src/lib/components/code-block.svelte
@@ -0,0 +1,10 @@
+<script lang="ts" module>
+	import { createHighlighter } from 'shiki/index.mjs';
+	import { createJavaScriptRegexEngine } from 'shiki/index.mjs';
+
+	const highlighter = await createHighlighter({
+		themes: ['catppuccin-macchiato'],
+		langs: ['javascript', 'typescript', 'python'],
+		engine: createJavaScriptRegexEngine()
+	});
+</script>
