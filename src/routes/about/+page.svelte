98eca91e1eb0a811339691a4f6b4fd27c906975f
alyx <alexanderbek3@gmail.com>
2026-01-12 10:29:42 +1030

    article pages and formatting for about/links

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,31 +2,33 @@
 	<title>Confused Ramblings - About</title>
 </svelte:head>
 
-<div class="container h-full mx-auto flex p-10">
-	<div class="space-y-5">
-		<h1 class="h1">About</h1>
-		<p>Hello, I'm ash (they/them).</p>
-		<p>
-			I Needed a spot to throw shit when I felt like putting it in a public place. The code is
-			available on my github. This website is currently built using a combination of:
-		</p>
-		<ul class="list">
-			<li><a href="https://svelte.dev">Svelte</a></li>
-			<li><a href="https://skeleton.dev">Skeleton</a></li>
-			<li><a href="https://neocities.org">Neocities</a></li>
-		</ul>
-		<p>
-			Posts are tagged, this does something now. Clicking on a tag will filter for posts with that
-			tag.
-		</p>
+<div class="content">
+	<article>
+		<div class="m-5">
+			<h1 class="h1">About</h1>
+			<p>Hello, I'm ash (they/she).</p>
+			<p>
+				I Needed a spot to throw shit when I felt like putting it in a public place. The code is
+				available on my github. This website is currently built using a combination of:
+			</p>
+			<ul class="list">
+				<li><a href="https://svelte.dev">Svelte</a></li>
+				<li><a href="https://skeleton.dev">Skeleton</a></li>
+				<li><a href="https://neocities.org">Neocities</a></li>
+			</ul>
+			<p>
+				Posts are tagged, this does something now. Clicking on a tag will filter for posts with that
+				tag.
+			</p>
 
-		<p>
-			This site also has an rss feed at <a
-				href="https://scylla.neocities.org/rss.xml"
-				data-sveltekit-preload-data="off">/rss.xml</a
-			>
-		</p>
-	</div>
+			<p>
+				This site also has an rss feed at <a
+					href="https://scylla.neocities.org/rss.xml"
+					data-sveltekit-preload-data="off">/rss.xml</a
+				>
+			</p>
+		</div>
+	</article>
 </div>
 
 <style>
---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,48 @@
+<svelte:head>
+	<title>Confused Ramblings - Links</title>
+</svelte:head>
+
+<div class="content">
+	<article>
+		<div class="m-5">
+			<h1 class="h1">Links</h1>
+			<p>Some random links to cool shit/friends blogs</p>
+			<div class="prose">
+				<ul class="list">
+					<li>
+						<a href="https://uuupah.neocities.org/">uuupah, who convinced me to hop on neocities</a>
+					</li>
+					<li><a href="https://garthrey.neocities.org/">Garthrey, cool bike, cool person.</a></li>
+					<li><a href="https://neocities.org">Neocities</a></li>
+				</ul>
+				<p>
+					Posts are tagged, this does something now. Clicking on a tag will filter for posts with
+					that tag.
+				</p>
+
+				<p>
+					This site also has an rss feed at <a
+						href="https://scylla.neocities.org/rss.xml"
+						data-sveltekit-preload-data="off">/rss.xml</a
+					>
+				</p>
+			</div>
+		</div>
+	</article>
+</div>
+
+<style>
+	a {
+		text-decoration: underline;
+	}
+
+	ul {
+		list-style-type: disc;
+		padding-left: 2rem;
+		margin-block: 1em;
+	}
+
+	li {
+		display: list-item;
+	}
+</style>
