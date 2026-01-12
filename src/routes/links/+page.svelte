86c22878f2fe3a615070151d30f00c8d3c90b078
alyx <alexanderbek3@gmail.com>
2026-01-12 19:02:20 +1030

    link updates

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -13,12 +13,8 @@
 						<a href="https://uuupah.neocities.org/">uuupah, who convinced me to hop on neocities</a>
 					</li>
 					<li><a href="https://garthrey.neocities.org/">Garthrey, cool bike, cool person.</a></li>
-					<li><a href="https://neocities.org">Neocities</a></li>
+					<li><a href="https://mause.me">Elli, all round gem of a human and python nerd</a></li>
 				</ul>
-				<p>
-					Posts are tagged, this does something now. Clicking on a tag will filter for posts with
-					that tag.
-				</p>
 
 				<p>
 					This site also has an rss feed at <a
