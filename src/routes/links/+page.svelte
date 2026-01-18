b9a8568237883888c14527bc6e079dbb8ca51dae
Ash <51521812+thembo-sec@users.noreply.github.com>
2026-01-19 00:38:29 +1030

    Update +page.svelte

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -14,8 +14,8 @@
 					</li>
 					<li><a href="https://garthrey.neocities.org/">Garthrey, cool bike, cool person.</a></li>
 					<li><a href="https://mause.me">Elli, all round gem of a human and python nerd</a></li>
-					<li><a href="https://thescreamingwall.com">Canberra based photographer, great zines</a></li>
-    <li><a href="https://certifikate.io">I hope you like nixos and functional programming!</a></li>
+					<li><a href="https://thescreamingwall.com">The Screaming Wall: Canberra based photographer, great zines</a></li>
+    <li><a href="https://certifikate.io">Kate: I hope you like nixos and functional programming!</a></li>
 				</ul>
 
 				<p>
